491c701d3d4e211837d70997cbdfd3eb7e11504a
joshua-baum <joshuabaum@berkeley.edu>
2020-10-21 14:33:55 -0700

    finished vs

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -10,7 +10,7 @@ export default function HomeBody(props) {
             <HomeBar text="Photography" color="#774E29" dark="#442509"/>
             <HomeBar text="The Lives Lost" color="#A46A26" dark="#834802"/>
             <HomeBar text="My Favorite Things" color="#C39D63" dark="#937952"/>
-            <HomeBar text="" color="#E0C379" />
+            <HomeBar text="" color="#E0C379" dark="#E0C379" />
         </div>
     );
 }
