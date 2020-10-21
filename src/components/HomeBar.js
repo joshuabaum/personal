6b2e4a28fa9ed9138d97f2ee2f824952f62dec9a
joshua-baum <joshuabaum@berkeley.edu>
2020-10-21 14:28:19 -0700

    pretty solid

--- src/components/HomeBar.js
+++ src/components/HomeBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/homePage.css';
 
-export default function HomeBar(props) {
-    return (
-        <div className="bar" style={{ backgroundColor: props.color, transform: `rotate(8deg)`}}>
-            <h2 className="bar-text">{props.text}</h2>
-        </div>
-    );
+
+
+export default function HomeBar(props){
+        const [color, setColor] = React.useState(props.color);
+        const styles = {
+            'background-color': color,
+            'transform': `rotate(8deg) translate(-2vw, 0)`
+        }
+        return (
+            <div className="bar" style={styles}
+                onMouseEnter={() => setColor(props.dark)}
+                onMouseLeave={() => setColor(props.color)}
+            >
+                <h2 className="bar-text">{props.text}</h2>
+            </div>
+        );
 }
