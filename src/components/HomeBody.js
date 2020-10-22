449d8db64a989bcab027c1a4971669057006497e
joshua-baum <joshuabaum@berkeley.edu>
2020-10-21 21:13:11 -0700

    sick colors

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -6,11 +6,11 @@ import HomeBar from './HomeBar';
 export default function HomeBody(props) {
     return (
         <div className="home-body">
-            <HomeBar text="Resume" color="#82C7D7" dark="#06ACD0"/>
-            <HomeBar text="Photography" color="#774E29" dark="#442509"/>
-            <HomeBar text="The Lives Lost" color="#A46A26" dark="#834802"/>
-            <HomeBar text="My Favorite Things" color="#C39D63" dark="#937952"/>
-            <HomeBar text="" color="#E0C379" dark="#E0C379" />
+            <HomeBar text="Resume" color="#82C7D7" dark="#74555d"/>
+            <HomeBar text="Photography" color="#c8bb6d" dark="#74555d"/>
+            <HomeBar text="The Lives Lost" color="#ffa800" dark="#74555d"/>
+            <HomeBar text="My Favorite Things" color="#c47d5d" dark="#74555d"/>
+            <HomeBar text="" color="#a25328" dark="#E0C379" />
         </div>
     );
 }
